1a62fe2ea7551621471414000b4040e593b31683
mhf <1603291350@qq.com>
2021-02-24 23:00:17 +0800

    添加项目文件。

---
+++ QueryExcel/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace QueryExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.查询模式 = new System.Windows.Forms.Label();
+            this.but_Clear = new System.Windows.Forms.Button();
+            this.richTextBox_input = new System.Windows.Forms.RichTextBox();
+            this.but_query = new System.Windows.Forms.Button();
+            this.but_select = new System.Windows.Forms.Button();
+            this.comb_mode = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.IsBalloon = true;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 45);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.splitContainer1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.查询模式);
+            this.splitContainer2.Panel2.Controls.Add(this.but_Clear);
+            this.splitContainer2.Panel2.Controls.Add(this.richTextBox_input);
+            this.splitContainer2.Panel2.Controls.Add(this.but_query);
+            this.splitContainer2.Panel2.Controls.Add(this.but_select);
+            this.splitContainer2.Panel2.Controls.Add(this.comb_mode);
+            this.splitContainer2.Size = new System.Drawing.Size(892, 488);
+            this.splitContainer2.SplitterDistance = 640;
+            this.splitContainer2.TabIndex = 5;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.richTextBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(640, 488);
+            this.splitContainer1.SplitterDistance = 317;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(317, 488);
+            this.treeView1.TabIndex = 0;
+            this.toolTip1.SetToolTip(this.treeView1, "添加的文件！");
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(319, 488);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.toolTip1.SetToolTip(this.richTextBox1, "显示查询的结果！");
+            // 
+            // 查询模式
+            // 
+            this.查询模式.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.查询模式.AutoSize = true;
+            this.查询模式.Location = new System.Drawing.Point(24, 299);
+            this.查询模式.Name = "查询模式";
+            this.查询模式.Size = new System.Drawing.Size(77, 14);
+            this.查询模式.TabIndex = 5;
+            this.查询模式.Text = "查询模式：";
+            // 
+            // but_Clear
+            // 
+            this.but_Clear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.but_Clear.Location = new System.Drawing.Point(170, 340);
+            this.but_Clear.Name = "but_Clear";
+            this.but_Clear.Size = new System.Drawing.Size(75, 23);
+            this.but_Clear.TabIndex = 4;
+            this.but_Clear.Text = "清空";
+            this.but_Clear.UseVisualStyleBackColor = true;
+            this.but_Clear.Click += new System.EventHandler(this.but_Clear_Click);
+            // 
+            // richTextBox_input
+            // 
+            this.richTextBox_input.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox_input.Location = new System.Drawing.Point(2, 5);
+            this.richTextBox_input.Name = "richTextBox_input";
+            this.richTextBox_input.Size = new System.Drawing.Size(219, 265);
+            this.richTextBox_input.TabIndex = 0;
+            this.richTextBox_input.Text = "";
+            this.toolTip1.SetToolTip(this.richTextBox_input, "输入需要查询的信息！");
+            // 
+            // but_query
+            // 
+            this.but_query.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.but_query.Location = new System.Drawing.Point(89, 340);
+            this.but_query.Name = "but_query";
+            this.but_query.Size = new System.Drawing.Size(75, 23);
+            this.but_query.TabIndex = 3;
+            this.but_query.Text = "查询";
+            this.but_query.UseVisualStyleBackColor = true;
+            this.but_query.Click += new System.EventHandler(this.but_query_Click);
+            // 
+            // but_select
+            // 
+            this.but_select.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.but_select.Location = new System.Drawing.Point(8, 340);
+            this.but_select.Name = "but_select";
+            this.but_select.Size = new System.Drawing.Size(75, 23);
+            this.but_select.TabIndex = 1;
+            this.but_select.Text = "选择文件";
+            this.but_select.UseVisualStyleBackColor = true;
+            this.but_select.Click += new System.EventHandler(this.but_select_Click);
+            // 
+            // comb_mode
+            // 
+            this.comb_mode.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.comb_mode.FormattingEnabled = true;
+            this.comb_mode.Items.AddRange(new object[] {
+            "所有文件",
+            "同级路径文件",
+            "选中文件"});
+            this.comb_mode.Location = new System.Drawing.Point(107, 296);
+            this.comb_mode.Name = "comb_mode";
+            this.comb_mode.Size = new System.Drawing.Size(109, 22);
+            this.comb_mode.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.comb_mode, "所有文件：查询当前文件夹及子文件夹下的所有文件。\r\n同级路径文件：只查询当前文件夹内文件，不查询子文件夹下文件。\r\n选中文件：查询文件树中选中的文件。");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 533);
+            this.Controls.Add(this.splitContainer2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ShowFormShadow = true;
+            this.Text = "Excel查询工具";
+            this.TitleBarColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Controls.SetChildIndex(this.splitContainer2, 0);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label 查询模式;
+        private System.Windows.Forms.Button but_Clear;
+        private System.Windows.Forms.RichTextBox richTextBox_input;
+        private System.Windows.Forms.Button but_query;
+        private System.Windows.Forms.Button but_select;
+        private System.Windows.Forms.ComboBox comb_mode;
+    }
+}
+
---
+++ QueryExcel/Form1.cs
@@ -0,0 +1,363 @@
+﻿using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using MControl.Forms;
+
+namespace QueryExcel
+{
+    public partial class Form1 : MFormWithTitle
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comb_mode.SelectedIndex = 0;
+        }
+
+        #region 选择文件路径
+        string g_sTreeListPath = "";
+        private void but_select_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                folderBrowserDialog1.SelectedPath = g_sTreeListPath;
+                folderBrowserDialog1.Description = "选择回放数据总文件夹";
+                if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    g_sTreeListPath = folderBrowserDialog1.SelectedPath;
+                }
+                else
+                {
+                    return;
+                }
+
+                if (g_sTreeListPath != "")
+                {
+                    new Thread(new ThreadStart(() =>
+                    {
+                        PaintTreeView(treeView1, g_sTreeListPath);
+                    })).Start();
+                }
+                else
+                {
+                    MessageBox.Show("请重新选择文件路径！", "提示");
+                }
+            }
+            catch
+            {
+                return;
+            }
+        }
+        #endregion
+
+        #region 生成程序所在根目录的TreeView
+        private void PaintTreeView(TreeView treeView, string fullPath)
+        {
+            try
+            {
+                treeView.Invoke(new Action(() =>
+                {
+                    treeView.Nodes.Clear(); //清空TreeView
+                }));
+                DirectoryInfo dirs = new DirectoryInfo(fullPath); //获得程序所在路径的目录对象
+                DirectoryInfo[] dir = dirs.GetDirectories();//获得目录下文件夹对象
+                FileInfo[] file = dirs.GetFiles();//获得目录下文件对象
+                int dircount = dir.Count();//获得文件夹对象数量
+                int filecount = file.Count();//获得文件对象数量
+                int sumcount = dircount + filecount;
+
+                if (sumcount == 0)
+                {
+                    MessageBox.Show("选择的路径下为空，请重新选择路径！");
+                    return;
+                }
+
+                //循环文件夹
+                for (int i = 0; i < dircount; i++)
+                {
+                    treeView.Invoke(new Action(() =>
+                    {
+                        treeView.Nodes.Add(dir[i].Name);
+                    }));
+                    string pathNode = fullPath + "\\" + dir[i].Name;
+                    GetMultiNode(treeView.Nodes[i], pathNode);
+                }
+
+                //循环文件
+                for (int j = 0; j < filecount; j++)
+                {
+                    if (file[j].Extension == ".xlsx" || file[j].Extension == ".xls")
+                    {
+                        treeView.Invoke(new Action(() =>
+                        {
+                            treeView.Nodes.Add(file[j].Name);
+                        }));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\r\n出错的位置为：Form1.PaintTreeView()");
+            }
+        }
+        #endregion
+
+        #region 遍历TreeView根节点下文件和文件夹
+        private bool GetMultiNode(TreeNode treeNode, string path)
+        {
+            if (Directory.Exists(path) == false)
+            {
+                return false;
+            }
+
+            DirectoryInfo dirs = new DirectoryInfo(path); //获得程序所在路径的目录对象
+            DirectoryInfo[] dir = dirs.GetDirectories();  //获得目录下文件夹对象
+            FileInfo[] file = dirs.GetFiles();            //获得目录下文件对象
+            int dircount = dir.Count();                   //获得文件夹对象数量
+            int filecount = file.Count();                 //获得文件对象数量
+            int sumcount = dircount + filecount;
+
+            if (sumcount == 0)
+            {
+                return false;
+            }
+
+            this.Invoke(new Action(() =>
+            {
+                treeNode.ImageIndex = 1;
+                treeNode.SelectedImageIndex = 1;
+            }));
+
+            //循环文件夹
+            for (int j = 0; j < dircount; j++)
+            {
+                this.Invoke(new Action(() =>
+                {
+                    treeNode.Nodes.Add(dir[j].Name);
+                }));
+                string pathNodeB = path + "\\" + dir[j].Name;
+                GetMultiNode(treeNode.Nodes[j], pathNodeB);
+            }
+
+            this.Invoke(new Action(() =>
+            {
+                treeNode.ImageIndex = 0;
+                treeNode.SelectedImageIndex = 0;
+            }));
+
+            //循环文件
+            for (int j = 0; j < filecount; j++)
+            {
+                if (file[j].Extension == ".xlsx" || file[j].Extension == ".xls")
+                {
+                    this.Invoke(new Action(() =>
+                    {
+                        treeNode.Nodes.Add(file[j].Name);
+                    }));
+                }
+            }
+            return true;
+        }
+        #endregion
+
+        #region 开始查询
+        string[] g_strInput;
+        bool g_bQuit = false;                                //停止查询
+        int g_nComb_mode = 0;                                //查询文件的模式
+        private void but_query_Click(object sender, EventArgs e)
+        {
+            if (but_query.Text == "查询")
+            {
+                g_bQuit = false;
+                g_strInput = richTextBox_input.Lines;                          //记录需要查询的文本
+                g_nComb_mode = comb_mode.SelectedIndex;                        //记录查询模式
+                string l_strSelected = "";
+                if(g_nComb_mode == 2)
+                {
+                    try
+                    {
+                        l_strSelected = treeView1.SelectedNode.Text;                //记录选中节点
+                    }
+                    catch (NullReferenceException ne)
+                    {
+                        richTextBox1.AppendText("未选中文件！\n");
+                        return;
+                    }
+                }
+                
+                but_query.Text = "停止";
+
+                new Thread(new ThreadStart(() =>
+                {
+                    //只查询选中的一个文件
+                    if (g_nComb_mode == 2)
+                    {
+                        if (l_strSelected.Contains(".xlsx"))
+                        {
+                            string l_sfullpath = g_sTreeListPath + "\\" + treeView1.SelectedNode.FullPath;
+                            Query(l_sfullpath);
+                        }
+                        return;
+                    }
+
+                    //查询多个文件
+                    for (int i = 0; i < treeView1.Nodes.Count; i++)
+                    {
+                        FindNode(treeView1.Nodes[i]);
+                        if (g_bQuit)
+                        {
+                            break;
+                        }
+                    }
+                    this.Invoke(new Action(() =>
+                    {
+                        but_query.Text = "查询";
+                    }));
+                })).Start();
+            }
+            else
+            {
+                g_bQuit = true;
+            }
+        }
+        #endregion
+
+        #region 遍历文件
+        private void FindNode(TreeNode p_tn)
+        {
+            if (p_tn == null) return;
+
+            //查询当前路径下所有文件
+            if (p_tn.Text.Contains(".xlsx"))
+            {
+                string l_sfullpath = g_sTreeListPath + "\\" + p_tn.FullPath;
+                Query(l_sfullpath);
+            }
+
+            //查询当前文件夹和子文件夹下的所有文件
+            if (g_nComb_mode != 0)
+            {
+                return;
+            }
+            foreach (TreeNode tn in p_tn.Nodes)
+            {
+                if (p_tn.Text.Contains(".xlsx"))
+                {
+                    string l_sfullpath = g_sTreeListPath + "\\" + p_tn.FullPath;
+                    Query(l_sfullpath);
+                }
+                FindNode(tn);
+                if (tn != null) continue;
+            }
+            return;
+        }
+        #endregion
+
+        #region 打开文件开始查询
+        private void Query(string p_strPath)
+        {
+            try
+            {
+                FileStream fsRead = OpenLocalFile(p_strPath, FileMode.Open, FileAccess.Read);
+                if (fsRead == null)
+                {
+                    this.Invoke(new Action(() =>
+                    {
+                        richTextBox1.AppendText("\n打开失败：" + Path.GetFileName(p_strPath) + "\n");
+                    }));
+                    return;
+                }
+                XSSFWorkbook workbook = new XSSFWorkbook(fsRead);
+                fsRead.Close();
+                this.Invoke(new Action(() =>
+                {
+                    richTextBox1.AppendText("\n打开表格：" + Path.GetFileName(p_strPath) + "\n");
+                }));
+
+                for (int i = 0; i < workbook.NumberOfSheets; i++)
+                {
+                    ISheet sheet = workbook.GetSheetAt(i);
+                    for (int j = 0; j < sheet.LastRowNum; j++)
+                    {
+                        IRow row = sheet.GetRow(j);
+                        foreach (ICell cell in row.Cells)
+                        {
+                            foreach (string l_str in g_strInput)
+                            {
+                                if (l_str == "")
+                                {
+                                    continue;
+                                }
+                                if (cell.ToString().Contains(l_str))
+                                {
+                                    this.Invoke(new Action(() =>
+                                    {
+                                        richTextBox1.SelectionFont = new Font("黑体", 9f, FontStyle.Regular);
+                                        richTextBox1.SelectionColor = Color.DodgerBlue;
+                                        richTextBox1.AppendText(l_str + "位于：" + sheet.SheetName + "->第" + (j + 1) + "行" + "\n");
+                                    }));
+                                }
+                            }
+                        }
+                        if (g_bQuit)
+                        {
+                            return;
+                        }
+                    }
+                }
+                workbook.Close();
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+        #endregion
+
+        #region 判断文件是否被占用并打开
+        /// <summary>
+        /// 判断文件是否被占用并打开，如果被占用则提示关闭并重新判断
+        /// </summary>
+        /// <param name="strPath"></param>
+        /// <param name="fileMode"></param>
+        /// <param name="fileAccess"></param>
+        /// <returns>返回null则文件被占用，未被占用则返回打开的文件</returns>
+        public FileStream OpenLocalFile(string strPath, FileMode fileMode, FileAccess fileAccess)
+        {
+            FileStream stream = null;
+            for (int i = 0; i < 3; i++)
+            {
+                try
+                {
+                    stream = new FileStream(strPath, fileMode, fileAccess);
+                    return stream;
+                }
+                catch
+                {
+                    MessageBox.Show("文件 [" + strPath + "] 在其它地方被打开，请关闭文件后点击确认！");
+                    if (i == 2)
+                    {
+                        MessageBox.Show("请检查文件是否正常或删除并重新添加文件后再尝试！");
+                    }
+                }
+            }
+            return stream;
+        }
+        #endregion
+
+        #region 清空
+        private void but_Clear_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+        #endregion
+    }
+}
